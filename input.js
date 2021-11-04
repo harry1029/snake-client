3b37eb014f434901a74e08267549648edba20592
Harry Jiang <harry10291993@hotmail.com>
2021-11-03 20:14:17 -0400

    Implement input listener allowing keys to send directional input to server

--- input.js
+++ input.js
@@ -1,6 +1,11 @@
 // setup interface to handle user input from stdin
 
-const setupInput = function () {
+const { connect } = require("./client");
+
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -14,6 +19,22 @@ const handleUserInput = function(key) {
   if (key === '\u0003') {
     console.log("Game terminated manually");
     process.exit();
+  }
+
+  if (key === 'w') {
+    connection.write("Move: up");
+  }
+
+  if (key === 'a') {
+    connection.write("Move: left");
+  }
+
+  if (key === 's') {
+    connection.write("Move: down");
+  }
+
+  if (key === 'd') {
+    connection.write("Move: right");
   }
 };
 
